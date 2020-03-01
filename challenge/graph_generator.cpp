8319be1d5331024995b6cdeb4673cb3dfbe893ad
bobbyshashin <shashin1011@gmail.com>
2020-02-29 20:21:22 -0500

    Fix abs->fabs bug

--- challenge/graph_generator.cpp
+++ challenge/graph_generator.cpp
@@ -5,5 +5,5 @@ double GraphGenerator::calcDistance(double lat1, double lon1, double lat2,
                                     double lon2) const {
   return earth_radius_ * acos(sin(deg2Rad(lat1)) * sin(deg2Rad(lat2)) +
                               cos(deg2Rad(lat1)) * cos(deg2Rad(lat2)) *
-                                  cos(deg2Rad(abs(lon1 - lon2))));
+                                  cos(deg2Rad(fabs(lon1 - lon2))));
 }
